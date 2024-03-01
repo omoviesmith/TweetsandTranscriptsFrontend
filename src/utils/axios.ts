7bd2099d16d05e49ee3f590aebd7111000dc8675
omoviesmith <106410058+omoviesmith@users.noreply.github.com>
2024-03-01 09:52:11 +0100

    Update axios.ts

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 //
 const axiosInstance = axios.create({
-  //baseURL: "https://tweets-and-scripts2-0.onrender.com",
-  baseURL: "https://tweets-and-transcript.onrender.com",
+  baseURL: "https://tweets-and-scripts2-0.onrender.com",
+  //baseURL: "https://tweets-and-transcript.onrender.com",
   withCredentials: true,
 });
 
